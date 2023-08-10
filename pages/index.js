ea6fb1f717142c2a238e59c4fb6dffb269788c15
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-08-10 12:25:07 -0300

    refatorando array

---
+++ pages/index.js
@@ -0,0 +1,98 @@
+const generator = require("./textGenerator");
+require("dotenv").config({ path: "../.env" });
+const tmi = require("tmi.js");
+
+const reputation = {};
+const client = new tmi.Client({
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  identity: {
+    username: "taurediano",
+    password: process.env.TWITCH_OAUTH_TOKEN,
+  },
+  channels: [
+    "taurediano",
+    "k1notv",
+    "themalkavianx",
+    "granjas",
+    "bard0oo0",
+    "blacksmith_god",
+    "marjoux",
+    "xparchon",
+    "zeszin",
+  ],
+  // channels: ["taurediano"],
+});
+
+client.connect();
+
+client.on("message", (channel, tags, message, self) => {
+  if (self) {
+    return;
+  }
+  const baseg = [
+    "👍",
+    "🔪",
+    "✌️",
+    "🤞",
+    "🖖",
+    "🤘",
+    "🤙",
+    "💪",
+    "👈",
+    "👉",
+    "☝️",
+    "👆",
+    "🖕",
+    "👇",
+    "🖐️",
+    "✋",
+    "👌",
+    "👍",
+    "👎",
+    "✊",
+    "👊",
+    "🤛",
+    "🤜",
+    "🤚",
+    "👋",
+    "🤟",
+    "🤏",
+    "✍️",
+    "👏",
+    "👐",
+    "🙌",
+    "🤲",
+    "🥊",
+    "🙏",
+    "🤝",
+    "💅",
+    "🦾",
+    "Parabéns! Você encontrou um BASEG SHINY, você não ganhou absolutamente nada. BloodTrail ",
+  ];
+
+  let command = message.toLowerCase();
+  // console.log(command, "command")
+
+  if (command.includes("baseg")) {
+    client.say(
+      channel,
+      `Baseg ${baseg[Math.floor(Math.random() * baseg.length)]}`
+    );
+  }
+
+  if (command.includes("taurediano")) {
+    // (async () => {
+    //     client.say(channel, `@${tags.username}, ${await generator.generate(command)}`);
+    //   })();
+    client.say(
+      channel,
+      `${tags.username} Baseg ${
+        baseg[Math.floor(Math.random() * baseg.length)]
+      }`
+    );
+  }
+});
