eddcd2bb2526a47358d57f60524626eda164e3f7
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-08-10 11:57:44 -0300

    refatorando e pastas

---
+++ pages/index.js
@@ -0,0 +1,100 @@
+const generator = require("./textGenerator");
+require("dotenv").config({ path: "../.env" });
+const tmi = require("tmi.js");
+
+const reputation = {};
+const client = new tmi.Client({
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  identity: {
+    username: "taurediano",
+    password: process.env.TWITCH_OAUTH_TOKEN,
+  },
+  channels: [
+    "taurediano",
+    "k1notv",
+    "themalkavianx",
+    "granjas",
+    "bard0oo0",
+    "blacksmith_god",
+    "marjoux",
+    "xparchon",
+    "zeszin",
+  ],
+  // channels: ["taurediano"],
+});
+
+client.connect();
+
+client.on("message", (channel, tags, message, self) => {
+  if (self) {
+    return;
+  }
+  const baseg = [
+    "Baseg",
+    "Baseg 👍",
+    "Baseg 🔪",
+    "Baseg ✌️",
+    "Baseg 🤞",
+    "Baseg 🖖",
+    "Baseg 🤘",
+    "Baseg 🤙",
+    "Baseg 💪",
+    "Baseg 👈",
+    "Baseg 👉",
+    "Baseg ☝️",
+    "Baseg 👆",
+    "Baseg 🖕",
+    "Baseg 👇",
+    "Baseg 🖐️",
+    "Baseg ✋",
+    "Baseg 👌",
+    "Baseg 👍",
+    "Baseg 👎",
+    "Baseg ✊",
+    "Baseg 👊",
+    "Baseg 🤛",
+    "Baseg 🤜",
+    "Baseg 🤚",
+    "Baseg 👋",
+    "Baseg 🤟",
+    "Baseg 🤏",
+    "Baseg ✍️",
+    "Baseg 👏",
+    "Baseg 👐",
+    "Baseg 🙌",
+    "Baseg 🤲",
+    "Baseg 🥊",
+    "Baseg 🙏",
+    "Baseg 🤝",
+    "Baseg 💅",
+    "Baseg 🦾",
+    "Parabéns! Você encontrou um Baseg Shiny, você não ganhou absolutamente nada. BloodTrail ",
+  ];
+
+  let command = message.toLowerCase();
+  // console.log(command, "command")
+
+  if (command.includes("baseg")) {
+    client.say(channel, `${baseg[Math.floor(Math.random() * baseg.length)]}`);
+  }
+
+  if (command.includes("taured")) {
+    if (channel === "k1notv") {
+      client.say(channel, "TAURED");
+    }
+  }
+
+  if (command.includes("taurediano")) {
+    // (async () => {
+    //     client.say(channel, `@${tags.username}, ${await generator.generate(command)}`);
+    //   })();
+    client.say(
+      channel,
+      `${tags.username} ${baseg[Math.floor(Math.random() * baseg.length)]}`
+    );
+  }
+});
---
+++ pages/textGenerator.js
@@ -0,0 +1,22 @@
+require("dotenv").config({path: '../.env'})
+const got = require('got');
+
+async function generate(question) {
+  const url = 'https://api.openai.com/v1/engines/davinci/completions';
+  const prompt = `Baseg ${question}`
+  const params = {
+    "prompt": prompt,
+    "max_tokens": 160,
+    "temperature": 0.7,
+    "frequency_penalty": 0.5
+  };
+  const headers = {
+    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+  };
+
+  const response = await got.post(url, { json: params, headers: headers }).json();
+  output = `${prompt}${response.choices[0].text}`;
+  return output;
+}
+
+module.exports = { generate };
