d30c3075ff74c58990c7c4a10c3c9cabd88ef4dd
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-08-12 13:11:44 -0300

    new text gen func init

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 const generator = require("./textGenerator");
 require("dotenv").config({ path: "../.env" });
 const tmi = require("tmi.js");
+// import { Configuration, OpenAIApi } from "openai";
+// import readline from "readline";
 
-const reputation = {};
 const client = new tmi.Client({
   options: { debug: true },
   connection: {
@@ -13,18 +14,18 @@ const client = new tmi.Client({
     username: "taurediano",
     password: process.env.TWITCH_OAUTH_TOKEN,
   },
-  channels: [
-    "taurediano",
-    "k1notv",
-    "themalkavianx",
-    "granjas",
-    "bard0oo0",
-    "blacksmith_god",
-    "marjoux",
-    "xparchon",
-    "zeszin",
-  ],
-  // channels: ["taurediano"],
+  // channels: [
+  //   "taurediano",
+  //   "k1notv",
+  //   "themalkavianx",
+  //   "granjas",
+  //   "bard0oo0",
+  //   "blacksmith_god",
+  //   "marjoux",
+  //   "xparchon",
+  //   "zeszin",
+  // ],
+  channels: ["taurediano"],
 });
 
 client.connect();
@@ -85,14 +86,14 @@ client.on("message", (channel, tags, message, self) => {
   }
 
   if (command.includes("taurediano")) {
-    // (async () => {
-    //     client.say(channel, `@${tags.username}, ${await generator.generate(command)}`);
-    //   })();
-    client.say(
-      channel,
-      `${tags.username} Baseg ${
-        baseg[Math.floor(Math.random() * baseg.length)]
-      }`
-    );
+    (async () => {
+        client.say(channel, `@${tags.username}, ${await generator.generate(command)}`);
+      })();
+    // client.say(
+    //   channel,
+    //   `${tags.username} Baseg ${
+    //     baseg[Math.floor(Math.random() * baseg.length)]
+    //   }`
+    // );
   }
 });
--- pages/textGenerator.js
+++ pages/textGenerator.js
@@ -1,22 +1,53 @@
 require("dotenv").config({path: '../.env'})
 const got = require('got');
 
-async function generate(question) {
-  const url = 'https://api.openai.com/v1/engines/davinci/completions';
-  const prompt = `Baseg ${question}`
-  const params = {
-    "prompt": prompt,
-    "max_tokens": 160,
-    "temperature": 0.7,
-    "frequency_penalty": 0.5
-  };
-  const headers = {
-    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-  };
+const { Configuration, OpenAIApi } = require("openai");
 
-  const response = await got.post(url, { json: params, headers: headers }).json();
-  output = `${prompt}${response.choices[0].text}`;
-  return output;
+async function generate(input) {
+  // const url = 'https://api.openai.com/v1/engines/davinci/completions';
+  // const params = {
+  //   "prompt": prompt,
+  //   "max_tokens": 160,
+  //   "temperature": 0.7,
+  //   "frequency_penalty": 0.5
+  // };
+  // const headers = {
+  //   'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+  // };
+
+  // const response = await got.post(url, { json: params, headers: headers }).json();
+  // output = `${prompt}${response.choices[0].text}`;
+  // output = `${response.text}`
+  // console.log(response)
+  // return output;
+
+  const configuration = new Configuration({
+    organization: process.env.OPENAI_ORG_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  
+  const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  userInterface.prompt();
+  
+  userInterface.on("line", async (input) => {
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: input }],
+      })
+      .then((res) => {
+        console.log(res.data.choices[0].message.content);
+        userInterface.prompt();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
 }
 
 module.exports = { generate };
