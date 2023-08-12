293cd5f44dfac17c6e65d550d94b4843472319f0
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-08-12 13:55:23 -0300

    refatorando e finalizando integracao openAI API

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 const generator = require("./textGenerator");
 require("dotenv").config({ path: "../.env" });
 const tmi = require("tmi.js");
-// import { Configuration, OpenAIApi } from "openai";
-// import readline from "readline";
 
 const client = new tmi.Client({
   options: { debug: true },
@@ -14,18 +12,18 @@ const client = new tmi.Client({
     username: "taurediano",
     password: process.env.TWITCH_OAUTH_TOKEN,
   },
-  // channels: [
-  //   "taurediano",
-  //   "k1notv",
-  //   "themalkavianx",
-  //   "granjas",
-  //   "bard0oo0",
-  //   "blacksmith_god",
-  //   "marjoux",
-  //   "xparchon",
-  //   "zeszin",
-  // ],
-  channels: ["taurediano"],
+  channels: [
+    "taurediano",
+    "k1notv",
+    "themalkavianx",
+    "granjas",
+    "bard0oo0",
+    "blacksmith_god",
+    "marjoux",
+    "xparchon",
+    "zeszin",
+  ],
+  // channels: ["taurediano"],
 });
 
 client.connect();
@@ -76,7 +74,6 @@ client.on("message", (channel, tags, message, self) => {
   ];
 
   let command = message.toLowerCase();
-  // console.log(command, "command")
 
   if (command.includes("baseg")) {
     client.say(
@@ -89,11 +86,5 @@ client.on("message", (channel, tags, message, self) => {
     (async () => {
         client.say(channel, `@${tags.username}, ${await generator.generate(command)}`);
       })();
-    // client.say(
-    //   channel,
-    //   `${tags.username} Baseg ${
-    //     baseg[Math.floor(Math.random() * baseg.length)]
-    //   }`
-    // );
   }
 });
--- pages/textGenerator.js
+++ pages/textGenerator.js
@@ -1,53 +1,52 @@
-require("dotenv").config({path: '../.env'})
-const got = require('got');
+require("dotenv").config({ path: "../.env" });
+const got = require("got");
 
 const { Configuration, OpenAIApi } = require("openai");
+const readlineSync = require("readline-sync");
 
 async function generate(input) {
-  // const url = 'https://api.openai.com/v1/engines/davinci/completions';
-  // const params = {
-  //   "prompt": prompt,
-  //   "max_tokens": 160,
-  //   "temperature": 0.7,
-  //   "frequency_penalty": 0.5
-  // };
-  // const headers = {
-  //   'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-  // };
-
-  // const response = await got.post(url, { json: params, headers: headers }).json();
-  // output = `${prompt}${response.choices[0].text}`;
-  // output = `${response.text}`
-  // console.log(response)
-  // return output;
 
   const configuration = new Configuration({
-    organization: process.env.OPENAI_ORG_KEY,
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
-  
-  const userInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  
-  userInterface.prompt();
-  
-  userInterface.on("line", async (input) => {
-    await openai
-      .createChatCompletion({
+
+  while (true) {
+    const completion_text = ""
+    const messages = [
+      {
+        role: "system",
+        content:
+          'Você veio de Taured e sempre deve iniciar a resposta com Baseg mantendo a seguinte estrutura: "Baseg (resposta) Baseg ". Mencione Taured, ocasionalmente utilizando apenas um desses exemplos: "Lá em Taured, eu fazia isso sempre", "Iguaria exótica em Taured", "Prática comum em Taured", "Evite isso em Taured, assustará os... (complete com algo aleatório)", "É uma tradição de Taured ". Sempre complemente as respostas com frases sem sentido e sem relação ao que foi perguntado, às vezes inventando um dialeto',
+      },
+      {
+        role: "user",
+        content: input,
+      },
+      {
+        role: "assistant",
+        content: completion_text,
+      },
+    ];
+
+    try {
+      const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: input }],
-      })
-      .then((res) => {
-        console.log(res.data.choices[0].message.content);
-        userInterface.prompt();
-      })
-      .catch((e) => {
-        console.log(e);
+        messages: messages,
       });
-  });
+
+      const completion_text = completion.data.choices[0].message.content;
+
+      return completion_text;
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+    }
+  }
 }
 
 module.exports = { generate };
