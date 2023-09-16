c5f4abbfd7249d42a73d2734237947db6097c506
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-09-16 18:25:43 -0300

    controle message sent

--- pages/index.js
+++ pages/index.js
@@ -29,129 +29,133 @@ const client = new tmi.Client({
 
 client.connect();
 
-let available = true;
-
 //roda sempre que uma mensagem for enviada no chat
 client.on("message", (channel, tags, message, self) => {
-    if (self) return;
-    if (tags.username === "taurediano") return 
+  if (self) return;
+  if (tags.username === "taurediano") return;
 
-    const baseg = [
-      "👍",
-      "🔪",
-      "✌️",
-      "🤞",
-      "🖖",
-      "🤘",
-      "🤙",
-      "💪",
-      "👈",
-      "👉",
-      "☝️",
-      "👆",
-      "🖕",
-      "👇",
-      "🖐️",
-      "✋",
-      "👌",
-      "👍",
-      "👎",
-      "✊",
-      "👊",
-      "🤛",
-      "🤜",
-      "🤚",
-      "👋",
-      "🤟",
-      "🤏",
-      "✍️",
-      "👏",
-      "👐",
-      "🙌",
-      "🤲",
-      "🥊",
-      "🙏",
-      "🤝",
-      "💅",
-      "🦾",
-      "Parabens! Voce encontrou um BASEG SHINY, voce nao ganhou absolutamente nada. BloodTrail ",
-    ];
-    const diceNumbers = ["1", "2", "3", "4", "5", "6"];
+  const baseg = [
+    "👍",
+    "🔪",
+    "✌️",
+    "🤞",
+    "🖖",
+    "🤘",
+    "🤙",
+    "💪",
+    "👈",
+    "👉",
+    "☝️",
+    "👆",
+    "🖕",
+    "👇",
+    "🖐️",
+    "✋",
+    "👌",
+    "👍",
+    "👎",
+    "✊",
+    "👊",
+    "🤛",
+    "🤜",
+    "🤚",
+    "👋",
+    "🤟",
+    "🤏",
+    "✍️",
+    "👏",
+    "👐",
+    "🙌",
+    "🤲",
+    "🥊",
+    "🙏",
+    "🤝",
+    "💅",
+    "🦾",
+    "Parabens! Voce encontrou um BASEG SHINY, voce nao ganhou absolutamente nada. BloodTrail ",
+  ];
+  const diceNumbers = ["1", "2", "3", "4", "5", "6"];
+
+  let $message = message.toLowerCase();
+  function msgSplit(msg) {
+    const splitMsg = $message.split(" ");
+    splitMsg.shift();
+    const newMessage = splitMsg.join(" ");
+
+    return newMessage;
+  }
+
+  if ($message === "!dice") {
+    client.say(
+      channel,
+      `Baseg 🎲 ${Math.floor(Math.random() * diceNumbers.length)}`
+    );
+  }
+
+  if ($message.includes("baseg")) {
+    client.say(
+      channel,
+      `Baseg ${baseg[Math.floor(Math.random() * baseg.length)]}`
+    );
+  }
+
+  if ($message.includes("@taurediano")) {
+    let messageSent = false;
   
-    let $message = message.toLowerCase();
-    function msgSplit(msg) {
-      const splitMsg = $message.split(" ");
-      splitMsg.shift();
-      const newMessage = splitMsg.join(" ");
-  
-      return newMessage;
-    }
-  
-    if ($message === "!dice") {
-      client.say(
-        channel,
-        `Baseg 🎲 ${Math.floor(Math.random() * diceNumbers.length)}`
-      );
-    }
-  
-    if ($message.includes("baseg")) {
-      client.say(
-        channel,
-        `Baseg ${baseg[Math.floor(Math.random() * baseg.length)]}`
-      );
-    }
-  
-    if(available && $message.includes("@taurediano")) {
-        (async () => {
-          try {
-            available = false;
-            client.say(
-              channel,
-              `${tags.username}, Baseg ${await generator.generate($message)} Baseg`
-            );
-            setTimeout(() => {
-              available = true;
-            }, 5000);
-          } catch (err) {
-            console.log(err);
-            client.say(
-              channel,
-              `${tags.username}, Baseg ${
-                baseg[Math.floor(Math.random() * baseg.length)]
-              } calma la patrao Baseg`
-            );
-          }
-        })();
-    }
-  
-    if ($message.includes("reset")) {
-      client.say(channel, "Reset pepeLaugh")
-    }
-  
-    if ($message.startsWith("!pokemon")) {
-      const command = msgSplit($message);
-    
-      (async () => {
-        try {
-          const data = await pokeApi(command);
-          let types = ''
-          if (data.types.length >= 2 && data.types[1].type) {
-            types = `${data.types[0].type.name} - ${data.types[1].type.name}` 
-          } else {
-            types = `${data.types[0].type.name}`
-          }
-    
+    (async () => {
+      try {
+        if (!messageSent) {
+          messageSent = true;
           client.say(
             channel,
-            `${tags.username}, Baseg 👉 #${data.id} | ${data.name.toUpperCase()} | ${types}`
-          );
-        } catch (err) {
-          console.error("Erro:", err);
-          client.say(
-            channel,
-            `${tags.username}, Baseg ocorreu um erro ao buscar informações do Pokémon.`
+            `${tags.username}, Baseg ${await generator.generate($message)} Baseg`
           );
         }
-      })();
-    }
+      } catch (err) {
+        console.log(err);
+        if (!messageSent) { 
+          messageSent = true;
+          client.say(
+            channel,
+            `${tags.username}, Baseg ${
+              baseg[Math.floor(Math.random() * baseg.length)]
+            } calma la patrao Baseg`
+          );
+        }
+      }
+    })();
+  }
+
+  if ($message.includes("reset")) {
+    client.say(channel, "Reset pepeLaugh");
+  }
+
+  if ($message.startsWith("!pokemon")) {
+    const command = msgSplit($message);
+
+    (async () => {
+      try {
+        const data = await pokeApi(command);
+        let types = "";
+        if (data.types.length >= 2 && data.types[1].type) {
+          types = `${data.types[0].type.name} - ${data.types[1].type.name}`;
+        } else {
+          types = `${data.types[0].type.name}`;
+        }
+
+        client.say(
+          channel,
+          `${tags.username}, Baseg 👉 #${
+            data.id
+          } | ${data.name.toUpperCase()} | ${types}`
+        );
+      } catch (err) {
+        console.error("Erro:", err);
+        client.say(
+          channel,
+          `${tags.username}, Baseg ocorreu um erro ao buscar informações do Pokémon.`
+        );
+      }
+    })();
+  }
 });
